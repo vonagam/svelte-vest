30d9d7c1a118a497b16a85a5996c64e8f381dee1
Dmitrii Maganov <vonagam@gmail.com>
2023-09-18 15:55:20 +0300

    Pass a value as a second argument of test function

--- src/Test.ts
+++ src/Test.ts
@@ -4,17 +4,17 @@ import {Access} from "./Access.js";
 export declare namespace Test {
   type Enforced = ReturnType<typeof Vest["enforce"]>;
   type Enforce = (message: string) => Enforced;
-  type Body = (enforce: Enforce) => void | Promise<void>;
+  type Body<T = any> = (enforce: Enforce, value: T) => void | Promise<void>;
 }
 
-export type Test<V = any, A = V> = (field: Access.Field<A>, test: Test.Body) => void;
+export type Test<V = any, A = V> = <F extends Access.Field<A>>(field: F, test: Test.Body<A[F]>) => void;
 
 export const Test = <V, A = V>(values: V, get: Access.Get<V, A>) => {
   const test: Test<V, A> = (field, run) => {
     Vest.test(field, () => {
       const value = get(values, field);
       const enforce = (message: string) => Vest.enforce(value).message(message);
-      return run(enforce);
+      return run(enforce, value);
     });
   };
 
