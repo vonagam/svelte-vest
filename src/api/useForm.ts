e37d4be1e6a95183d1498fc27d29243b08f231ef
Dmitrii Maganov <vonagam@gmail.com>
2023-10-01 06:26:10 +0300

    Add `options.context` for `useForm` to skip setting context

--- src/api/useForm.ts
+++ src/api/useForm.ts
@@ -2,12 +2,16 @@ import * as Svelte from "svelte";
 import {FormApi} from './FormApi.js';
 
 export declare namespace useForm {
-  type Options<V = any, A = V> = FormApi.Options<V, A>;
+  type Options<V = any, A = V> = FormApi.Options<V, A> & {context?: boolean};
 }
 
 export const useForm = <V = any, A = V>(options: useForm.Options<V, A>) => {
   const form = new FormApi(options);
-  Svelte.setContext(useForm, form);
+
+  if (options.context !== false) {
+    Svelte.setContext(useForm, form);
+  }
+
   return form;
 };
 
